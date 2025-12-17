3725e7265a08cd2fd6dd577d692539391c0cd08c
bala-amypo <bala@amypo.in>
2025-12-17 04:39:29 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -12,6 +12,28 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @Column
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.ANALYST;
+
+    public enum Role{
+        ADMIN,
+        ANALYST
+    }
+
+    //Constructors
+    public User(){}
+
+    public User(String name , String name , String email , String password , Role role)
+    {
+        
+    }
+
 
 }
