dd0742425321b5dbff58df50f02e51742b458151
bala-amypo <bala@amypo.in>
2025-12-17 08:32:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ShiftTemplate.java
@@ -0,0 +1,110 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "shift_templates")
+public class ShiftTemplate{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.STAFF;
+
+    @Column(nullable = false)
+    private String skills;
+
+    @Column(nullable = false)
+    private Integer maxWeeklyHours;
+
+    @Column(nullable = false , updatable = false)
+    private LocalDateTime createdAt;
+
+    public enum Role {
+        ADMIN,
+        STAFF
+    }
+
+    //Constructors
+    public ShiftTemplate(){
+        this.createdAt=LocalDateTime.now();
+    }
+
+    public ShiftTemplate(String fullName , String email , Role role , String skills , Integer maxWeeklyHours)
+    {
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+        this.skills = skills;
+        this.maxWeeklyHours = maxWeeklyHours;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    //Getters
+    public Long getId() 
+    {
+        return id;
+    }
+    public String getFullName()
+    {
+        return fullName; 
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public Role getRole()
+    {
+        return role;
+    }
+    public String getSkills()
+    {
+        return skills;
+    }
+    public Integer getMaxWeeklyHours()
+    {
+        return maxWeeklyHours;
+    }
+    public LocalDateTime getCreatedAt()
+    {
+        return createdAt;
+    }
+    //Setters
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+    public void setFullName(String fullName)
+    {
+        this.fullName = fullName;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public void setRole(Role role)
+    {
+        this.role = role;
+    }
+    public void setSkills(String skills)
+    {
+        this.skills = skills;
+    }
+    public void setMaxWeeklyHours(Integer maxWeeklyHours)
+    {
+        this.maxWeeklyHours = maxWeeklyHours;
+    }
+    public void setCreatedAt(LocalDateTime createdAt)
+    {
+        this.createdAt = createdAt;
+    }
+}
