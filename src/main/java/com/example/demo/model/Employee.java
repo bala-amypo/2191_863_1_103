2e5220fe3290ab624bcc8ac82aeeedef32dec7da
bala-amypo <bala@amypo.in>
2025-12-17 05:19:29 +0000

    User commit

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -23,6 +23,32 @@ public class Employees {
     @Column(nullable = false)
     private String skills;
 
-    @Column
+    @Column(nullable = false)
+    private Integer maxWeeklyHours;
+
+    @Column(nullable = false , updateable = false)
+    private LocalDateTime createdAt;
+
+    public enum Role {
+        ADMIN,
+        STAFF
+    }
+
+    //Constructors
+    public Employees(){
+        this.createdAt=LocalDateTime.now();
+    }
+
+    public Employees(String fullName , String email , Role role , String skills , Integer maxWeeklyHours)
+    {
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+        this.skills = skills;
+        this.maxWeeklyHours = maxWeeklyHours;
+        this.
+    }
+
+
 
 }
