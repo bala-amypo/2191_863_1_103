937b5f57b94cbe3589202ac4fd320edee5b3c1cb
bala-amypo <bala@amypo.in>
2025-12-19 06:12:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/GeneratedShiftScheduleRepository.java
+++ src/main/java/com/example/demo/repository/GeneratedShiftScheduleRepository.java
@@ -4,9 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.GeneratedShiftSchedule;
 
-@Repository
 import java.util.List;
-import.java.time.LocalDate;
+import java.time.LocalDate;
 
 @Repository
 public interface GeneratedShiftScheduleRepository extends JpaRepository<GeneratedShiftSchedule,Long>
