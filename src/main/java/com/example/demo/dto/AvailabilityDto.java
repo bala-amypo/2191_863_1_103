aae8375c4ed4a0ae8c56d7af89c347930a3b2fc0
bala-amypo <bala@amypo.in>
2025-12-19 08:24:17 +0000

    User commit

--- src/main/java/com/example/demo/dto/AvailabilityDto.java
+++ src/main/java/com/example/demo/dto/AvailabilityDto.java
@@ -1,1 +1,12 @@
 package com.example.demo.dto;
+
+import lombok.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvailabilityDto {
+    private Boolean available;
+    private LocalDate availableDate;
+}
