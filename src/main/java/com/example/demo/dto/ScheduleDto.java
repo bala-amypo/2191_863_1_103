d9193f643074b531cab0647610348be3eb00ee34
bala-amypo <bala@amypo.in>
2025-12-19 08:42:16 +0000

    User commit

--- src/main/java/com/example/demo/dto/ScheduleDto.java
+++ src/main/java/com/example/demo/dto/ScheduleDto.java
@@ -1,2 +1,16 @@
+package com.example.demo.dto;
 
-LocalDate shiftDate , LocalTime startTime , LocalTime endTime , Department department ,  Employee employee , ShiftTemplate shiftTemplate
+import lombok.*;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShiftTemplateDto {
+    private SLocalDate shiftDate;
+    private LocalTime startTime ;
+    private LocalTime endTime;
+    private Department department;
+    private Employee employee;
+    private ShiftTemplate shiftTemplate;
+}
