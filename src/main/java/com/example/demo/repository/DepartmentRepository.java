15d81f74c9b3b2f1cec6925f8d0057490c6e6570
bala-amypo <bala@amypo.in>
2025-12-19 05:10:16 +0000

    User commit

--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -1,7 +1,16 @@
-package com.example.demo.repository;
+ackage com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.model.Employee;
+import com.example.demo.model.Department;
+
+import java.util.Optional;
+import java.util.List;
 
 @Repository
+public interface UserRepository extends JpaRepository<Employee,Long>
+{
+    Optional<Employee>findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<Employee> findAll();
+}
--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -2,13 +2,15 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.model.Emplo;
+import com.example.demo.model.Employee;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long>
+public interface EmployeeRepository extends JpaRepository<Employee,Long>
 {
-    Optional<User>findByEmail(String email);
+    Optional<Employee>findByEmail(String email);
     boolean existsByEmail(String email);
+    List<Employee> findAll();
 }
