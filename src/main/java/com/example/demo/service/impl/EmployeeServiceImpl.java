62d103d4071d715c67906929889969fe91dc7bf4
bala-amypo <bala@amypo.in>
2025-12-20 11:34:29 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.EmployeeAvailability;
 import com.example.demo.repository.AvailabilityRepository;
-import com.example.demo.repository.EmployeeRepository; 
+import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.service.AvailabilityService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
