7f9b62388f50a2bc2db9678ee9a91685f5996327
bala-amypo <bala@amypo.in>
2025-12-26 06:12:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/AvailabilityRepository.java
+++ src/main/java/com/example/demo/repository/AvailabilityRepository.java
@@ -1,18 +1,15 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.EmployeeAvailability;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.model.EmployeeAvailability;
-
 import java.time.LocalDate;
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface AvailabilityRepository extends JpaRepository<EmployeeAvailability,Long>
-{
-    Optional<EmployeeAvailability> findByEmployee_IdAndAvailableDate(Long id , LocalDate date);
-    List<EmployeeAvailability> findByAvailableDateAndAvailable(LocalDate date, boolean available);
-    List<EmployeeAvailability> findByEmployee_Id(Long id);
-    List<EmployeeAvailability> findAll();
+public interface AvailabilityRepository extends JpaRepository<EmployeeAvailability, Long> {
+    Optional<EmployeeAvailability> findByEmployee_IdAndAvailableDate(Long employeeId, LocalDate date);
+    List<EmployeeAvailability> findByAvailableDateAndAvailable(LocalDate date, Boolean available);
+    List<EmployeeAvailability> findByEmployee_Id(Long employeeId);
 }
